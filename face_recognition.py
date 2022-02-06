d7d6aee746e1b01ecae7d32f19a3494f04dd7b6a
Gabriel <gabriel.angelo1008@gmail.com>
2022-02-06 10:33:03 -0300

    creating person dir

--- face_recognition.py
+++ face_recognition.py
@@ -3,14 +3,20 @@ import os
 TRAINING_SET_DIR = './Training Set'
 
 print('Checking Training Set folder existence...')
-if(os.path.isdir(TRAINING_SET_DIR)):
+if os.path.isdir(TRAINING_SET_DIR):
     print('Found Training Set folder\n')
 else:
-    print('Folder not found. Creating directory...')
+    print('Folder not found. Making directory...')
     os.mkdir(TRAINING_SET_DIR)
-    print('Created Training Set folder')
+    print('Created Training Set folder\n')
 
-name = input('Type the name of the person to be used for training the classifier (0 to finish): ')
+name = input('Type the name of the person to be used for training the classifier (0 to skip): ')
 while name != '0':
-    print(name)
+    user_path = TRAINING_SET_DIR + '/' + name
+    if os.path.isdir(user_path):
+        print('\nPerson already in the training set')
+    else:
+        print('\nUser does not exist in the database. Making directory...')
+        os.mkdir(user_path)
+        print('Created ' + name + ' folder in the training set\n')
     name = input('Type the name of the person to be used for training the classifier (0 to finish): ')
