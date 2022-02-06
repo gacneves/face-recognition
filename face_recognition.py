3cc676acf5112b3e47d288f994d7ab208daec9cf
Gabriel <gabriel.angelo1008@gmail.com>
2022-02-06 10:51:27 -0300

    option take photos

--- face_recognition.py
+++ face_recognition.py
@@ -15,8 +15,17 @@ while name != '0':
     user_path = TRAINING_SET_DIR + '/' + name
     if os.path.isdir(user_path):
         print('\nPerson already in the training set')
+        decision = input('Do you want to retake the frames (y/n): ')
+        while decision != 'y' and decision != 'n':  
+            decision = input('Type a valid option. Do you want to retake the frames (y/n): ')
+        take_photos = True if decision == 'y' else False if decision == 'n' else 0
     else:
         print('\nUser does not exist in the database. Making directory...')
         os.mkdir(user_path)
         print('Created ' + name + ' folder in the training set\n')
+        take_photos = True
+
+    if take_photos:
+        print('Take 100 photos')
+
     name = input('Type the name of the person to be used for training the classifier (0 to finish): ')
