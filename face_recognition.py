227863c6f27442b0917f38063053215872421c82
Gabriel <gabriel.angelo1008@gmail.com>
2022-02-06 10:01:13 -0300

    acquiring name of a person

---
+++ face_recognition.py
@@ -0,0 +1,4 @@
+name = input('Type the name of the person to be used for training the classifier (0 to finish): ')
+while name != '0':
+    print(name)
+    name = input('Type the name of the person to be used for training the classifier (0 to finish): ')
