560bf3a9c1bab3bf4314817d0348761549521146
gacneves <gabriel.angelo1008@gmail.com>
2022-02-08 20:35:16 -0300

    prediction made by trained model

--- face_recognition.py
+++ face_recognition.py
@@ -13,8 +13,8 @@ def readCSVFiles(images, labels):
                 reader = csv.reader(csvfile)
                 for row in reader:
                     list = row[0].split(sep=';')
-                    images.append(cv2.imread(list[0]))
-                    labels.append(list[1])
+                    images.append(cv2.imread(list[0], 0))
+                    labels.append(int(list[1]))
     print('\n Successfully read training set...')
 
 # Initialize application
@@ -27,13 +27,44 @@ while decision != 'y' and decision != 'n':
 if decision == 'y':
     manage_training_set.manageTrainingSet()
 
+# Generate csv files for the training set users
 manage_training_set.generateCSVFile()
 
 images, labels = [], []
 readCSVFiles(images, labels)
 
-(images, labels) = [numpy.array(lis, dtype="object") for lis in [images, labels]]
+# Converting lists to numpy.arrays
+(images, labels) = [numpy.array(lis) for lis in [images, labels]]
 
-# Training EigenFaces model
-# model = cv2.face.LBPHFaceRecognizer_create()
-# model.train(images,labels)
+# Training FisherFace model
+model = cv2.face.FisherFaceRecognizer_create()
+model.train(images,labels)
+model.setThreshold(1000)
+
+# Open Haar cascade classifier for identifying face
+cascade_face_file = os.path.join(manage_training_set.CASCADE_CLASSIFIER_DIR, 'haarcascade_frontalface_default.xml')
+face_cascade = cv2.CascadeClassifier(cascade_face_file)
+
+# Open camera
+camera = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+if not camera.isOpened():
+    print('\n Error: Could not open the camera')
+    exit()
+print(' Successfully opened the camera')
+
+while True:
+    # Take camera frame
+    _, frame = camera.read()
+
+    # Convert RGB color channels to gray
+    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    # Detect face in the frame
+    faces = face_cascade.detectMultiScale(gray_frame)
+    for face in faces:
+        x, y, w, h = face
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (255,0,0), 2)
+        face_image = gray_frame[y:y+h, x:x+w]
+        face_image = cv2.resize(face_image, (manage_training_set.OUTPUT_WIDTH, manage_training_set.OUTPUT_HEIGHT))
+        # Try recognizing the face
+        prediction = model.predict(face_image)
+        
